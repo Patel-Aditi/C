26e483fd5d937d7d3a8ad163bbd7ab4fe212b1cf
Aditi Patel <aditi3010patel@gmail.com>
2026-04-13 21:26:30 +0530

    array

---
+++ lect13/linearsearch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int arr[5]={9,2,3,5,8};
+int target=5;
+for(int i=0;i<5;i++){
+    if(arr[i]==target){
+    printf("%d,i");
+  }
+} 
+ return 0;
+}
---
+++ lect13/slidingwindow.c
@@ -0,0 +1,23 @@
+//sliding window of sum of array 
+//using single loop
+#include <stdio.h>
+int main(){
+int arr[6]={1,2,3,4,5,6};
+int k=3;
+int n= sizeof(arr)/sizeof (arr[0]);
+int windowsum=0;
+for(int i=0;i<k;i++){
+    windowsum=windowsum+arr[i];
+}
+
+int maxsum=windowsum;
+
+for(int j=k;j<n;j++){
+    windowsum=windowsum+arr[j]-arr[j-k];
+    if(windowsum>maxsum){
+       maxsum=windowsum;
+    }
+  }
+  printf("%d",maxsum);
+ return 0;
+}
